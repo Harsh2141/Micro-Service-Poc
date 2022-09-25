798a914520b6a3fd06d9bef9ca4a237bca665f1b
Jasmin Kachhadiya <272311@ODCDPW.IC>
2022-09-25 12:55:03 +0530

    swagger implimentation

--- api-gateway/src/main/java/com/tender247/poc/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/tender247/poc/ApiGatewayApplication.java
@@ -3,12 +3,16 @@ package com.tender247.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 import reactivefeign.spring.config.EnableReactiveFeignClients;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableReactiveFeignClients
+@EnableSwagger2
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
@@ -16,4 +20,9 @@ public class ApiGatewayApplication {
 		System.out.println("Git Test");
 	}
 	
+	@Bean
+	UiConfiguration uiConfig() {
+		return new UiConfiguration("validatorUrl", "list", "alpha", "schema",
+				UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, true, 60000L);
+	}
 }
